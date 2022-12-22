5a728f1d5e52f7ca68380ddf8a85e05f89ed9dcd
Katarzyna Tomczak <katarzyna.ntomczak@gmail.com>
2022-12-22 22:29:25 +0100

    @task GIR-28 modify .gitignore file

--- src/main/java/com/gt/gamexchanger/GameXChangerApplication.java
+++ src/main/java/com/gt/gamexchanger/GameXChangerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GameXChangerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GameXChangerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GameXChangerApplication.class, args);
+    }
 
 }
