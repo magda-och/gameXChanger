1f66f0ebad4a43bc5c94af99aa08c3d229588195
Katarzyna Tomczak <katarzyna.ntomczak@gmail.com>
2022-12-22 18:21:55 +0100

    Initial commit

---
+++ src/main/java/com/gt/gamexchanger/GameXChangerApplication.java
@@ -0,0 +1,13 @@
+package com.gt.gamexchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameXChangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameXChangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gt/gamexchanger/GameXChangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gt.gamexchanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameXChangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
