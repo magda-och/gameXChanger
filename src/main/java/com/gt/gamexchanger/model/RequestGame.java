1a6226b182ef1885e3c8b1280b7f73959f4d8f98
MikolajczykDominika <mikolajczyk.dominika94@gmail.com>
2023-01-17 11:04:36 +0100

    GIR-87 Added requestGameList to user

--- src/main/java/com/gt/gamexchanger/model/RequestGame.java
+++ src/main/java/com/gt/gamexchanger/model/RequestGame.java
@@ -1,5 +1,7 @@
 package com.gt.gamexchanger.model;
 
+import com.gt.gamexchanger.enums.RequestStatus;
+
 public class RequestGame {
     private RequestStatus requestStatus;
     private User fromUser;
--- src/main/java/com/gt/gamexchanger/model/User.java
+++ src/main/java/com/gt/gamexchanger/model/User.java
@@ -1,9 +1,12 @@
 package com.gt.gamexchanger.model;
 
+import java.util.List;
+
 public class User {
     private String name;
     private String lastName;
     private String email;
     private String password;
     private Shelf gamesShelf;
+    private List<RequestGame> requestGameList;
 }
