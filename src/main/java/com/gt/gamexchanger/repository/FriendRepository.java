e5bb3c92a3f488c8eabc1582372128369d31d5b5
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-01-17 14:47:22 +0100

    GIR-83 Add Friend Repository implementation

--- src/main/java/com/gt/gamexchanger/repository/FriendRepository.java
+++ src/main/java/com/gt/gamexchanger/repository/FriendRepository.java
@@ -4,10 +4,17 @@ import com.gt.gamexchanger.model.Friend;
 import com.gt.gamexchanger.model.User;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface FriendRepository {
     boolean existByFirstUserAndSecondUser(User first, User second);
 
     List<Friend> findByFirstUser(User user);
     List<Friend> findBySecondUser(User user);
+
+    void saveFriend(Friend friend);
+    List<Friend> getFriends();
+    Friend getFriendById(Long id);
+    void removeFriendById(Long id);
+
 }
--- src/main/java/com/gt/gamexchanger/repository/FriendRepositoryImp.java
+++ src/main/java/com/gt/gamexchanger/repository/FriendRepositoryImp.java
@@ -3,9 +3,14 @@ package com.gt.gamexchanger.repository;
 import com.gt.gamexchanger.model.Friend;
 import com.gt.gamexchanger.model.User;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 public class FriendRepositoryImp implements FriendRepository {
+
+    private final Map<Long, Friend> inMemoryFriends = new HashMap<>();
     @Override
     public boolean existByFirstUserAndSecondUser(User first, User second) {
         return false;
@@ -19,5 +24,26 @@ public class FriendRepositoryImp implements FriendRepository {
     @Override
     public List<Friend> findBySecondUser(User user) {
         return null;
+    }
+
+    @Override
+    public void saveFriend(Friend friend) {
+        friend.setId((long) (inMemoryFriends.size()+1));
+        inMemoryFriends.put(friend.getId(), friend);
+    }
+
+    @Override
+    public List<Friend> getFriends() {
+        return null;
+    }
+
+    @Override
+    public Friend getFriendById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void removeFriendById(Long id) {
+
     }
 }
