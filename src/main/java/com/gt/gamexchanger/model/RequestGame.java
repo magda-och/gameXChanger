2654a60d81ed3f4e8c46d14d12be2463805ef417
MikolajczykDominika <mikolajczyk.dominika94@gmail.com>
2023-01-17 11:01:33 +0100

    GIR-87 Added RequestGame model

---
+++ src/main/java/com/gt/gamexchanger/model/RequestGame.java
@@ -0,0 +1,9 @@
+package com.gt.gamexchanger.model;
+
+public class RequestGame {
+    private RequestStatus requestStatus;
+    private User fromUser;
+    private User toUser;
+    private Game game;
+
+}
