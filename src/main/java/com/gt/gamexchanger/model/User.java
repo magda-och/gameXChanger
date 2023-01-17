e03a09a7ccd7d61048edd4c7a64642ca329f9741
RVQB87 <magdalena.och1@gmail.com>
2023-01-17 10:20:14 +0100

    Added user model

---
+++ src/main/java/com/gt/gamexchanger/model/User.java
@@ -0,0 +1,8 @@
+package com.gt.gamexchanger.model;
+
+public class User {
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/gt/gamexchanger/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.gt.gamexchanger.repository;
+
+public interface UserRepository {
+}
