f5d62f7d659a92038c7a940da504537a34fba134
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-01-18 12:27:14 +0100

    GIR-83 Implemented methods to FriendRepositoryImp

--- src/main/java/com/gt/gamexchanger/repository/FriendRepositoryImp.java
+++ src/main/java/com/gt/gamexchanger/repository/FriendRepositoryImp.java
@@ -3,27 +3,31 @@ package com.gt.gamexchanger.repository;
 import com.gt.gamexchanger.model.Friend;
 import com.gt.gamexchanger.model.User;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class FriendRepositoryImp implements FriendRepository {
 
     private final Map<Long, Friend> inMemoryFriends = new HashMap<>();
     @Override
     public boolean existByFirstUserAndSecondUser(User first, User second) {
-        return false;
+        return inMemoryFriends.values().stream()
+                .allMatch(friend -> friend.getFirstUser().equals(first)
+                        && friend.getSecondUser().equals(second));
     }
 
     @Override
     public List<Friend> findByFirstUser(User user) {
-        return null;
+        return inMemoryFriends.values().stream()
+                .filter(friend -> friend.getFirstUser().equals(user))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Friend> findBySecondUser(User user) {
-        return null;
+        return inMemoryFriends.values().stream()
+                .filter(friend -> friend.getSecondUser().equals(user))
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -34,16 +38,16 @@ public class FriendRepositoryImp implements FriendRepository {
 
     @Override
     public List<Friend> getFriends() {
-        return null;
+        return new ArrayList<>(inMemoryFriends.values());
     }
 
     @Override
     public Friend getFriendById(Long id) {
-        return null;
+        return inMemoryFriends.get(id);
     }
 
     @Override
     public void removeFriendById(Long id) {
-
+        inMemoryFriends.remove(id);
     }
 }
