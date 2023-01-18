cd554d7174b4ab79b1e7b3c8868f126cfa530919
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-01-19 00:42:28 +0100

    Gir-85 Add getAllFriend endpoint

--- src/main/java/com/gt/gamexchanger/controller/FriendController.java
+++ src/main/java/com/gt/gamexchanger/controller/FriendController.java
@@ -1,9 +1,12 @@
 package com.gt.gamexchanger.controller;
 
 import com.gt.gamexchanger.dto.FriendDto;
+import com.gt.gamexchanger.dto.UserDto;
 import com.gt.gamexchanger.service.FriendService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/friends")
@@ -14,10 +17,15 @@ public class FriendController {
     public FriendController(FriendService friendService) {
         this.friendService = friendService;
     }
+
     @PostMapping("/add")
     public String addNewFriend(@RequestBody FriendDto friendDto){
         friendService.saveFriend(friendDto);
         return "New friend are successfully created";
     }
 
+    @GetMapping("/get/{id}")
+    public List<UserDto> getAllFriends(@PathVariable Long id){
+        return friendService.getFriends(id);
+    }
 }
