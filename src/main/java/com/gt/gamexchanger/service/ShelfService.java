a3a74b55c29d704a9693cd073a40a4e1ae6caa65
MikolajczykDominika <mikolajczyk.dominika94@gmail.com>
2023-01-19 12:04:24 +0100

    GIR-117 Added shelf methods

--- src/main/java/com/gt/gamexchanger/service/ShelfService.java
+++ src/main/java/com/gt/gamexchanger/service/ShelfService.java
@@ -1,22 +1,31 @@
 package com.gt.gamexchanger.service;
 
+import com.gt.gamexchanger.dto.GameDto;
 import com.gt.gamexchanger.mapper.DtoMapper;
-import com.gt.gamexchanger.model.Shelf;
-import com.gt.gamexchanger.model.ShelfDto;
+import com.gt.gamexchanger.model.*;
 import com.gt.gamexchanger.repository.GameRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ShelfService {
     private final GameRepository gameRepository;
-    private final DtoMapper<ShelfDto, Shelf> dtoMapper;
+    private final DtoMapper<GameDto, Game> dtoMapper;
 
-    public ShelfService(GameRepository gameRepository, DtoMapper<ShelfDto, Shelf> dtoMapper) {
+    public ShelfService(GameRepository gameRepository, DtoMapper<GameDto, Game> dtoMapper) {
         this.gameRepository = gameRepository;
         this.dtoMapper = dtoMapper;
     }
-    //getAllmyGames-by my id
-    //getAllborrowedGame- by actual owner id
-    //
+    public List<GameDto> getAllMyGames(Long userId) {
+        return gameRepository.getMyGames(userId).stream()
+                .map(dtoMapper::toDto)
+                .toList();
+    }
+    public List<GameDto> getAllBorowedGame(Long userId) {
+        return gameRepository.getMyBorrowedGames(userId).stream()
+                .map(dtoMapper::toDto)
+                .toList();
+    }
 
 }
