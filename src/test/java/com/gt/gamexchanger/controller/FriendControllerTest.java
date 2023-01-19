ea40bd07799ebd8a31d0520a5df2402cfa4ac48b
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-01-19 11:43:41 +0100

    Gir-116 Add FriendControllerTest, FriendRepositoryImpTest, FriendServiceTest

---
+++ src/test/java/com/gt/gamexchanger/controller/FriendControllerTest.java
@@ -0,0 +1,16 @@
+package com.gt.gamexchanger.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FriendControllerTest {
+
+    @Test
+    void addNewFriend() {
+    }
+
+    @Test
+    void getAllFriends() {
+    }
+}
---
+++ src/test/java/com/gt/gamexchanger/repository/FriendRepositoryImpTest.java
@@ -0,0 +1,20 @@
+package com.gt.gamexchanger.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FriendRepositoryImpTest {
+
+    @Test
+    void saveFriend() {
+    }
+
+    @Test
+    void getFriends() {
+    }
+
+    @Test
+    void removeFriendById() {
+    }
+}
