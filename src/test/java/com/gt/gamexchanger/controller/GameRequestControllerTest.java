cec7f4306dd13ac3041f8f9e8519cd64b4852d1a
MikolajczykDominika <mikolajczyk.dominika94@gmail.com>
2023-01-19 21:46:33 +0100

    Added tests to GameRequestRepositoryImp

--- src/test/java/com/gt/gamexchanger/controller/GameRequestControllerTest.java
+++ src/test/java/com/gt/gamexchanger/controller/GameRequestControllerTest.java
@@ -1,4 +1,29 @@
 package com.gt.gamexchanger.controller;
 
+import org.junit.jupiter.api.Test;
+
 public class GameRequestControllerTest {
+    @Test
+    void getReqests() {
+    }
+
+    @Test
+    void addReqestGame() {
+    }
+
+    @Test
+    void deleteRequest() {
+    }
+
+    @Test
+    void updateRequest() {
+    }
+
+    @Test
+    void getSendReqests() {
+    }
+
+    @Test
+    void getReceivedReqests() {
+    }
 }
--- src/test/java/com/gt/gamexchanger/service/ShelfServiceTest.java
+++ src/test/java/com/gt/gamexchanger/service/ShelfServiceTest.java
@@ -1,4 +1,13 @@
 package com.gt.gamexchanger.service;
 
+import org.junit.jupiter.api.Test;
+
 public class ShelfServiceTest {
+    @Test
+    void getAllMyGames() {
+    }
+
+    @Test
+    void getAllBorowedGame() {
+    }
 }
