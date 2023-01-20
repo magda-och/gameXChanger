7404b7290ace2ae20079274fca71f297a1111aee
MikolajczykDominika <mikolajczyk.dominika94@gmail.com>
2023-01-20 11:56:11 +0100

    Added test to shelfController and gameRequestController

---
+++ src/test/java/com/gt/gamexchanger/mapper/ShelfDtoMapperTest.java
@@ -0,0 +1,16 @@
+package com.gt.gamexchanger.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShelfDtoMapperTest {
+
+    @Test
+    void toDto() {
+    }
+
+    @Test
+    void toDomainObject() {
+    }
+}
