17194e8793d9541245921557dd8c1bfc1cd2aa95
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-01-20 11:09:36 +0100

    Gir-116 Add FriendControllerTest controller

--- src/test/java/com/gt/gamexchanger/controller/FriendControllerTest.java
+++ src/test/java/com/gt/gamexchanger/controller/FriendControllerTest.java
@@ -1,8 +1,13 @@
 package com.gt.gamexchanger.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gt.gamexchanger.dto.FriendDto;
+import com.gt.gamexchanger.dto.GameDto;
 import com.gt.gamexchanger.model.Friend;
 import com.gt.gamexchanger.repository.FriendRepository;
+import com.gt.gamexchanger.service.FriendService;
+import com.gt.gamexchanger.service.GameService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,6 +15,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 //@WebMvcTest(FriendController.class)
 class FriendControllerTest {
 
@@ -21,13 +28,31 @@ class FriendControllerTest {
 
     @MockBean
     FriendRepository friendRepository;*/
-
+    FriendService friendService;
+    FriendControllerTest.UnderTest underTest;
+    FriendDto friendDto;
+    @BeforeEach
+    void setUp() {
+        friendDto = new FriendDto();
+        friendService = mock(FriendService.class);
+        underTest = new FriendControllerTest.UnderTest();
+    }
 
     @Test
     void addNewFriend() {
+            underTest.addNewFriend(friendDto);
+            verify(friendService, times(1)).saveFriend(friendDto);
     }
 
     @Test
     void getAllFriends() {
+        underTest.getAllFriends(friendDto.getFirstUserId());
+        verify(friendService, verificationData -> verificationData.getAllInvocations());
+    }
+
+    public class UnderTest extends FriendController {
+        public UnderTest() {
+            super(friendService);
+        }
     }
 }
