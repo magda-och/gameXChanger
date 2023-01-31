21145af4cc0fb9e59737025a89c02fec222b4698
RVQB87 <magdalena.och1@gmail.com>
2023-01-31 17:15:29 +0100

    Added methods to change password and update user

--- src/main/java/com/gt/gamexchanger/model/User.java
+++ src/main/java/com/gt/gamexchanger/model/User.java
@@ -14,9 +14,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(name = "First Name")
     private String firstName;
+    @Column(name = "Last Name")
     private String lastName;
+    @Column(name = "Email address")
     private String email;
+    @Column(name = "Password")
     private String password;
 
 }
