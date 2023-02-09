c3c608cb286748132ece67afb5626459383523e8
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-02-10 00:49:23 +0100

    Change RequestMapping path to /friends/requests

--- src/main/java/com/gt/gamexchanger/controller/FriendRequestController.java
+++ src/main/java/com/gt/gamexchanger/controller/FriendRequestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/requests")
+@RequestMapping("/friends/requests")
 public class FriendRequestController {
     private final FriendRequestService friendRequestService;
 
