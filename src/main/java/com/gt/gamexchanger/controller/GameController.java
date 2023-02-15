bd2a9a595e88d4f275d9c9fda1a46a8bde4f3d99
MikolajczykDominika <mikolajczyk.dominika94@gmail.com>
2023-02-15 13:45:07 +0100

    Added simple correct

--- src/main/java/com/gt/gamexchanger/controller/GameController.java
+++ src/main/java/com/gt/gamexchanger/controller/GameController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3100")
+@CrossOrigin
 @RestController
 @RequestMapping("/games")
 public class GameController {
