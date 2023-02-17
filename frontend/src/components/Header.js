89500de4777916500a4533b6ce89d252259c3a58
RVQB87 <magdalena.och1@gmail.com>
2023-02-17 11:52:10 +0100

    Correct leftsidebar color

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,7 +9,7 @@ function Header() {
                 <div className="container-fluid">
 
                     <a href="#" className="navbar-brand">
-                        <img src="https://bootstrapious.com/i/snippets/sn-nav-logo/logo.png" width="45" alt=""
+                        <img src="https://img.icons8.com/bubbles/512/ludo-game.png" width="45" alt=""
                              className="d-inline-block align-middle mr-2"/>
                             <span className="text-uppercase font-weight-bold">GameXChanger</span>
                     </a>
