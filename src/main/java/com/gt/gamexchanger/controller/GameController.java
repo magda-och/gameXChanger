a7f0ee9b9ba4eaa743c02aa510262db19fafd0f0
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-02-20 17:12:40 +0100

    Add cross-origin path to localhost:3000 and installing cors

--- src/main/java/com/gt/gamexchanger/controller/GameController.java
+++ src/main/java/com/gt/gamexchanger/controller/GameController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/games")
 public class GameController {
--- src/main/java/com/gt/gamexchanger/controller/UserController.java
+++ src/main/java/com/gt/gamexchanger/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/user")
 public class UserController {
