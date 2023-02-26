fb14c03135cb76baabd5c4dda81494116406e000
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-02-26 21:41:29 +0100

    Change of viewing FriendGames

--- frontend/src/components/Games/FriendGames.js
+++ frontend/src/components/Games/FriendGames.js
@@ -18,7 +18,33 @@ function FriendGames(id) {
     }, []);
 
     return (
-        <Games games = {friendGames}/>
+        <div>
+            <h2 className="text-center">Games</h2>
+            <table className="table table-striped">
+                <thead>
+                <tr>
+                    <td> Game name</td>
+                    <td> Game Visibility</td>
+                    <td> Game Status</td>
+                    <td> Contact</td>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    friendGames.map(game => {
+                            return <tr>
+                                <td> {game.id}</td>
+                                <td>{game.name}</td>
+                                <td> {game.visibility}</td>
+                                <td> {game.gameStatus}</td>
+                                <td> {game._owner}</td>
+                            </tr>
+                        }
+                    )
+                }
+                </tbody>
+            </table>
+        </div>
     )
 }
 
