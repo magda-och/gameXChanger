750e653bf835ee27500d4ad63240a7281d5a10d0
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-02-27 11:36:33 +0100

    Fix FriendGames - remove wrong return

--- frontend/src/components/Games/FriendGames.js
+++ frontend/src/components/Games/FriendGames.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import Games from "./Games";
 import {GameAPI} from "../../api/GameAPI";
 
 function FriendGames(id) {
@@ -17,41 +16,36 @@ function FriendGames(id) {
         });
     }, []);
 
-        return (
-            <div>
-                <h2 className="text-center">Games</h2>
-                <table className="table table-striped">
-                    <thead>
-                    <tr>
-                        <td> Game name</td>
-                        <td> Game Visibility</td>
-                        <td> Game Status</td>
-                        <td> Contact</td>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        friendGames.map(game => {
-                                return <tr key={game.id}>
-                                    <td> {game.id}</td>
-                                    <td>{game.name}</td>
-                                    <td> {game.visibility}</td>
-                                    <td> {game.gameStatus}</td>
-                                    <td> {game._owner}</td>
-                                </tr>
-                            }
-                        )
-                    }
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
     return (
-        <>
-            {displayFriendGames(props)}
-        </>
-    );
+        <div>
+            <h2 className="text-center">Games</h2>
+            <table className="table table-striped">
+                <thead>
+                <tr>
+                    <td> Game name</td>
+                    <td> Game Visibility</td>
+                    <td> Game Status</td>
+                    <td> Contact</td>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    friendGames.map(game => {
+                            return <tr key={game.id}>
+                                <td> {game.id}</td>
+                                <td>{game.name}</td>
+                                <td> {game.visibility}</td>
+                                <td> {game.gameStatus}</td>
+                                <td> {game._owner}</td>
+                            </tr>
+                        }
+                    )
+                }
+                </tbody>
+            </table>
+        </div>
+    )
 }
+
 
 export default FriendGames;
