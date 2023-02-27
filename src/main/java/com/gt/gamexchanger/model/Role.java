dcf71185c3ecbe921ab437d5cb1b8286e9713c82
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-02-27 23:06:57 +0100

    GIR-29 Add Role entity.

--- src/main/java/com/gt/gamexchanger/model/Role.java
+++ src/main/java/com/gt/gamexchanger/model/Role.java
@@ -1,4 +1,22 @@
 package com.gt.gamexchanger.model;
 
+import com.gt.gamexchanger.enums.RoleEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private RoleEnum name;
 }
