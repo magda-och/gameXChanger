7f7b52b0e955de2b323a73335b697f939daf2f1a
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-02-27 23:13:21 +0100

    GIR-29 Add findByName method to RoleRepository

--- src/main/java/com/gt/gamexchanger/repository/RoleRepository.java
+++ src/main/java/com/gt/gamexchanger/repository/RoleRepository.java
@@ -1,9 +1,13 @@
 package com.gt.gamexchanger.repository;
 
+import com.gt.gamexchanger.enums.RoleEnum;
 import com.gt.gamexchanger.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleEnum name);
 }
