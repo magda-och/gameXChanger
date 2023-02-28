178e6e8768e65e1b3ec6a6da3703f6dd903d96cf
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-02-28 19:57:58 +0100

    Add Optional to getUserById in UserController

--- src/main/java/com/gt/gamexchanger/controller/UserController.java
+++ src/main/java/com/gt/gamexchanger/controller/UserController.java
@@ -21,6 +21,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "http://localhost:3000")
