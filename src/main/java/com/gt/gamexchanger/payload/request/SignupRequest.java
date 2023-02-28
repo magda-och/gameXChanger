9c2e407cd5c01218fc2fc9feba0837e2ac132638
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-02-28 15:13:23 +0100

    Add SignupRequest and MessageResponse implementations

--- src/main/java/com/gt/gamexchanger/payload/request/SignupRequest.java
+++ src/main/java/com/gt/gamexchanger/payload/request/SignupRequest.java
@@ -10,15 +10,22 @@ import java.util.Set;
 @Getter
 @Setter
 public class SignupRequest {
-
+    @NotBlank
+    @Size(max = 30)
+    private String firstName;
+    @NotBlank
+    @Size(max = 30)
+    private String lastName;
     @NotBlank
     @Size(max = 50)
     @Email
     private String email;
-
-    private Set<String> role;
-
     @NotBlank
     @Size(min = 6, max = 40)
     private String password;
+    private String city;
+    private int phoneNumber;
+
+    //private Set<String> role;
+
 }
--- src/main/java/com/gt/gamexchanger/payload/response/MessageResponse.java
+++ src/main/java/com/gt/gamexchanger/payload/response/MessageResponse.java
@@ -1,10 +1,12 @@
 package com.gt.gamexchanger.payload.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class MessageResponse {
     private String message;
 }
