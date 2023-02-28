52552ff0446098d866389573f5afe99efa089954
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-02-28 11:14:29 +0100

    Gir-29 Add LoginRequest

--- src/main/java/com/gt/gamexchanger/payload/request/LoginRequest.java
+++ src/main/java/com/gt/gamexchanger/payload/request/LoginRequest.java
@@ -1,4 +1,15 @@
 package com.gt.gamexchanger.payload.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginRequest {
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String email;
 }
