816839880e40be7508c2cacd7f7cea7ccc8f09e9
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-02-28 10:15:26 +0100

    Gir-29 Add UserDetailsImpl and UserDetailsServiceImpl

--- src/main/java/com/gt/gamexchanger/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/gt/gamexchanger/security/services/UserDetailsServiceImpl.java
@@ -1,4 +1,26 @@
 package com.gt.gamexchanger.security.services;
 
-public class UserDetailsServiceImpl {
+import com.gt.gamexchanger.model.User;
+import com.gt.gamexchanger.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    UserRepository userRepository;
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + email));
+        return UserDetailsImpl.build(user);
+    }
 }
