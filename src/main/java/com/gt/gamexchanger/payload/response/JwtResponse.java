7e62f8192bf02f226a7cc6d2c7479f386ff4c819
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-03-01 11:36:26 +0100

    Add change to JwtResponse

--- src/main/java/com/gt/gamexchanger/payload/response/JwtResponse.java
+++ src/main/java/com/gt/gamexchanger/payload/response/JwtResponse.java
@@ -1,25 +1,26 @@
 package com.gt.gamexchanger.payload.response;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.gt.gamexchanger.enums.Role;
+import lombok.*;
 
-import java.util.List;
-@Getter
-@Setter
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class JwtResponse {
 
     private String token;
-    private String type = "Bearer";
+    private String type = "Bearer ";
     private Long id;
     private String username;
     private String email;
-    private List<String> roles;
+    private Role role;
 
-    public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles) {
+    public JwtResponse(String accessToken, Long id, String username, String email, Role role) {
         this.token = accessToken;
         this.id = id;
         this.username = username;
         this.email = email;
-        this.roles = roles;
+        this.role = role;
     }
 }
