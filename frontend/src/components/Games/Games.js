a3c809a07d4d9ea531a543193c94a770a403e6e9
MikolajczykDominika <mikolajczyk.dominika94@gmail.com>
2023-03-03 09:02:30 +0100

    fix games bug

--- frontend/src/components/Games/Games.js
+++ frontend/src/components/Games/Games.js
@@ -42,7 +42,6 @@ function Games(props) {
                         alert("You dont lent game")
                     }
                     window.location.replace('/profile/shelf');
-
                 });
         }else{
             GameAPI.update(id, status, 1)
