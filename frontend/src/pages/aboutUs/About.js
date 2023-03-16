a70966e16e9dd714be18dd21cab260e93553f578
RVQB87 <magdalena.och1@gmail.com>
2023-03-16 21:43:53 +0100

    Change about us

--- frontend/src/pages/aboutUs/About.js
+++ frontend/src/pages/aboutUs/About.js
@@ -4,6 +4,7 @@ import "./About.css";
 import dominikaImage from './img_1_1677745215179.jpg'
 import agaImage from './aga.jpg'
 import magdaImage from './20220929_110943.jpg'
+import usImage from './image6.jpeg'
 
 function About() {
     return (
@@ -17,37 +18,10 @@ function About() {
                         the four of us teamed up to create a unique board game exchange site.
                         After several months of hard work, we managed to create a fully functional application.</p>
                 </div>
-                <div className="row mt-3" align="left">
-                    <h3>Something more about our team...</h3>
-                </div>
-                <div className="row mt-2" align="left">
+                <div>
 
                     <div className="col-md-3">
-                        <img id="photoKasia" src={kasiaImage} alt="Kasia"/>
-                    </div>
-
-                    <div className="col-md-3" align="left">
-                        <h3>Kasia</h3>
-                    </div>
-                    <div className="col-md-3">
-                        <img id="photoKasia" src={agaImage} alt="Agas"/>
-                    </div>
-                    <div className="col-md-3">
-                        <h3> Agnieszka</h3>
-                    </div>
-                    <div className="row mt-5" align="left">
-                        <div className="col-md-3">
-                            <img id="photoKasia" src={magdaImage} alt="Kasia"/>
-                        </div>
-                        <div className="col-md-3" >
-                            <h3>Magda</h3>
-                        </div>
-                        <div className="col-md-3">
-                            <img id="photoDominika" src={dominikaImage} alt="Dominika"/>
-                        </div>
-                        <div className="col-md-3" >
-                            <h3>Dominika</h3>
-                        </div>
+                        <img id="usImage" src={usImage} alt="our-team"/>
                     </div>
 
                 </div>
