5a7156dbcc17cedd0139ffbd043c1b8055347398
Agnieszka Kusiak <agnieszkaczajk@gmail.com>
2023-03-20 14:48:28 +0100

    Change name of method remove to removeFriend

--- src/main/java/com/gt/gamexchanger/controller/UserController.java
+++ src/main/java/com/gt/gamexchanger/controller/UserController.java
@@ -68,8 +68,8 @@ public class UserController {
     }
 
     @DeleteMapping("friends/{userId}/{friendId}")
-    public ResponseEntity<?> remove(@PathVariable Long friendId,
-                                    @PathVariable Long userId) {
+    public ResponseEntity<?> removeFriend(@PathVariable Long friendId,
+                                          @PathVariable Long userId) {
         try{
             userService.deleteFriend(userId,friendId);
             return ResponseEntity.noContent().build();
